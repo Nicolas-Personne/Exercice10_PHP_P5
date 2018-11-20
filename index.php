8c22c2e8961d0c2aa8730ec2e8c260bd266a8317
root <personne.nico@gmail.com>
2018-11-20 15:52:41 +0100

    edit

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<body>
 <?php
 $deps = [
-  "02" => "Aisne",
-  "59" => "Nord",
-  "60" => "Oise",
-  "62" => "Pas-de-Calais",
-  "80" => "Somme"
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme'
 ];
-foreach($deps as $key=>$dep) {
-       echo "<li>"."Le département"." ".$dep." "."a le numéro"." ".$key."</li>";
+foreach($deps as $key=>$dep) {?>
+  <li><?php echo 'Le département'.' '.$dep.' '.'a le numéro'.' '.$key ?></li>
+  <?php
 }
  ?>
+ <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+</body>
+</html>
